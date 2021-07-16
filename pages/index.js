682401b6a4b490fe6cf90064a70ebb289cefce76
Erik <kireSuomi@gmail.com>
2021-07-16 18:34:22 +0200

    Updated copy

--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,21 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Welcome</title>
 
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-          <Link href="/posts/first-post">
-            <a>this page!</a>
-          </Link>
+          Welcome to Erik Andersson
+      
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
         </p>
 
         <div className="grid">
